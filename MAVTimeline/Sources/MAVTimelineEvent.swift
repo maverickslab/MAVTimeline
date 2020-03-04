bf4e93ca0349287cfad3fc122cdaf260ed4f4143
Fernando Jorquera <fernando.jorquera@mavericks.cool>
2020-03-04 18:47:39 -0300

    event model

---
+++ MAVTimeline/Sources/MAVTimelineEvent.swift
@@ -0,0 +1,21 @@
+//
+//  MAVTimelineEvent.swift
+//  MAVTimeline
+//
+//  Created by Mavericks's iOS Dev on 04-03-20.
+//  Copyright © 2020 Mavericks. All rights reserved.
+//
+
+import Foundation
+class MAVTimelineEvent {
+    
+    //MARK: - Properties
+    ///date of event
+    public var date: Date
+    
+    //MARK: - Init
+    init(date: Date) {
+        self.date = date
+    }
+    
+}
