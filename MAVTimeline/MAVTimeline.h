5626bfe79101d9eb8ec02a6207388dbd94f83afa
Fernando Jorquera <fernando.jorquera@mavericks.cool>
2020-03-04 17:27:30 -0300

    Initial Commit

---
+++ MAVTimeline/MAVTimeline.h
@@ -0,0 +1,19 @@
+//
+//  MAVTimeline.h
+//  MAVTimeline
+//
+//  Created by Mavericks's iOS Dev on 04-03-20.
+//  Copyright © 2020 Mavericks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MAVTimeline.
+FOUNDATION_EXPORT double MAVTimelineVersionNumber;
+
+//! Project version string for MAVTimeline.
+FOUNDATION_EXPORT const unsigned char MAVTimelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAVTimeline/PublicHeader.h>
+
+
